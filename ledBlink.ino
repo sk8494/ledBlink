3ca6e5c245b034d3712713c9865e4543b8497a18
sk8494 <sk8494@nyu.edu>
2021-12-16 16:39:33 -0500

    added inverted LED pin

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,16 +1,20 @@
 int ledPin = 13;
-int ledPin = 14;
+int ledPin2 = 14;
+int ledPinInverted = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(ledPinInverted, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPinInverted, LOW);
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin, LOW);
+  digitalWrite(ledPinInverted, HIGH);
   delay(500);
 }
